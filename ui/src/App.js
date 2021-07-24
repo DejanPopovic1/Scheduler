ea721cd7d5423ec3ded6b404438a325628936c27
DejanPopovic <dejan@fennecgroup.co.za>
2021-07-24 21:46:43 +0200

    Routing to three pages and navbar implementation

--- ui/src/App.js
+++ ui/src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './Home';
+import Contact from './Contact';
+import Schedule from './Schedule';
 
 function App() {
   return (
     <main>
       <Switch>
           <Route path="/" component={Home} exact />
+          <Route path="/Contact" component={Contact} />
+          <Route path="/Schedule" component={Schedule} />
       </Switch>
     </main>
   );
