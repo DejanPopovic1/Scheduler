00aea6172f63fa26684cf527b7d3a4aa178a4b4e
DejanPopovic <dejan.dp.popovic@gmail.com>
2021-08-11 21:16:26 +0200

    added schedule model

---
+++ Scheduler/Models/Schedule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scheduler.Models
+{
+    public class Schedule
+    {
+        public DateTime scheduleDate { get; set; }
+        public string scheduleName { get; set; }
+        public float scheduleLat { get; set; }
+        public float scheduleLon { get; set; }
+    }
+}
