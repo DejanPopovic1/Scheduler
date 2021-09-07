7ebcde9f2c622d3b3293f0bbc8864720344fdc9c
DejanPopovic <dejan.dp.popovic@gmail.com>
2021-09-07 23:24:44 +0200

    posting data to login controller with refactoring error

--- Scheduler/ClientApp/src/login.js
+++ Scheduler/ClientApp/src/login.js
@@ -2,6 +2,18 @@
 import './login.css';
 
 class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.myVar = testFunc.bind(this);
+    }
+
+    postData = () => {
+        fetch("authenticate/login", {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ name: "John", city: "New York" })
+        });
+    }
     render() {
         return (
             <div id="background">
@@ -9,6 +21,7 @@ class Login extends React.Component {
                     <FormHeader title="Login" />
                     <LoginForm />
                     <OtherMethods />
+
                 </div>
             </div>
         )
@@ -21,22 +34,26 @@ const FormHeader = props => (
 
 const LoginForm = props => (
     <div>
-        <FormInput description="Username" placeholder="Enter your username" type="text" />
-        <FormInput description="Password" placeholder="Enter your password" type="password" />
-        <FormButton title="Log in" />
+        <form action="/LoginIndex" method="post">
+        <FormInput description="Username" placeholder="Enter your username" type="text" name="username" />
+        <FormInput description="Password" placeholder="Enter your password" type="password" name="password"/>
+            <FormButton title="Log in"/>
+        </form>
     </div>
 );
 
 const FormButton = props => (
     <div id="button" class="centeredRow">
         <button id="colourfulButton">{props.title}</button>
+        <input id="colourfulButton" type="submit" value={props.title} />
+        <input id="colourfulButton" type="submit" value="TEST"  />
     </div>
 );
 
 const FormInput = props => (
     <div class="centeredRow">
         <label id="colourfulLabel">{props.description}</label>
-        <input type={props.type} placeholder={props.placeholder} />
+        <input type={props.type} placeholder={props.placeholder} name={props.name} />
     </div>
 );
 
@@ -62,6 +79,11 @@ const Twitter = props => (
 const Google = props => (
     <a href="#" id="googleIcon"></a>
 );
+
+const testFunc = () => (
+    "test String"
+);
+
 
 export default Login
 
---
+++ Scheduler/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Scheduler.Models;
+
+namespace Scheduler.Controllers
+{
+    [ApiController]
+    [Route("authenticate")]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost("login")]
+        public int Login([FromBody] Example s)
+        {
+            //System.Environment.Exit(-1);
+            int test = 7;
+            return 5;
+        }
+
+        [HttpPost("postSchedule")]
+        public int postSchedule([FromBody] IEnumerable<Schedule> s)
+        {
+            //System.Environment.Exit(-1);
+            int test = 7;
+            return 5;
+        }
+    }
+}
+
