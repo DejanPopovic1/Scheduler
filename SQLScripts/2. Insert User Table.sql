9faf4fc85da6959901672c0e526fbc4b55830c5d
DejanPopovic <dejan.dp.popovic@gmail.com>
2021-09-10 20:33:07 +0200

    created user table and populated sample details

---
+++ SQLScripts/2. Insert User Table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE [User](
+	[Id] [nvarchar](450) NOT NULL,
+	[UserName] [nvarchar](256) NULL,
+	[PasswordHash] [nvarchar](max) NULL
+)
---
+++ SQLScripts/3. Hardcode a password for sample user.sql
@@ -0,0 +1,2 @@
+INSERT INTO [dbo].[User] (Id, UserName, PasswordHash)
+VALUES ('1', 'dejan.dp.popovic@gmail.com', '607e25882c603c0f515adfa322ef7f13');
