1d58d167db0ebdcdb8a7b4f5d075c014baeff037
DejanPopovic <dejan.dp.popovic@gmail.com>
2021-09-27 23:56:33 +0200

    upon succesful authentication, exit system

--- Scheduler/Controllers/LoginController.cs
+++ Scheduler/Controllers/LoginController.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Scheduler.Models;
 using Scheduler.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using System.Web;
 
 namespace Scheduler.Controllers
 {
@@ -22,8 +25,16 @@ namespace Scheduler.Controllers
         [HttpPost("login")]
         public int Login([FromBody] Example s)
         {
+            //System.Web.HttpContext.Current.Session["name"] = "Any Name";
+            //HttpContext context = HttpContext.Current.Session["ShoppingCart"];
             string hashedPassword = Cryptographic.CreateMD5Hash(s.password);
-            bool test2 = _dbContext.Users.Where(x => x.UserName == s.username && x.PasswordHash == hashedPassword).ToList().Any();
+            bool isCredentialsValid = _dbContext.Users.Where(x => EF.Functions.Collate(x.UserName, "SQL_Latin1_General_CP1_CS_AS") == s.username && x.PasswordHash == hashedPassword).Select(x => x).ToList().Any();
+            if (isCredentialsValid) 
+            {
+                System.Environment.Exit(-1);
+            }
+
+
             int test = 7;
             return 5;
         }
