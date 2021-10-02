b33b541735c6637a61c92ded325aebdbd775c88f
DejanPopovic <dejan.dp.popovic@gmail.com>
2021-10-02 21:17:35 +0200

    navbar formatting

--- Scheduler/ClientApp/src/components/NavigationBar.js
+++ Scheduler/ClientApp/src/components/NavigationBar.js
@@ -3,33 +3,37 @@ import { Nav, Navbar, Form, FormControl } from 'react-bootstrap';
 import styled from 'styled-components';
 import './NavBarStyle.css';
 
-const Styles = styled.div`
-  .navbar { background-color: #0066cc; }
-  a, .navbar-nav, .navbar-light .nav-link {
-    color: #9FFFCB;
-    &:hover { color: white; }
-  }
-  .navbar-brand {
-    font-size: 1.4em;
-    color: #9FFFCB;
-    &:hover { color: white; }
-  }
-`;
+//const Styles = styled.div`
+//  .navbar { background-color: #0066fc; }
+//  a, .navbar-nav, .navbar-light .nav-link {
+//    color: #9FFFCB;
+//    &:hover { color: white; }
+//  }
+//  .navbar-brand {
+//    font-size: 1.4em;
+//    color: #9FFFCB;
+//    &:hover { color: white; }
+//  }
+//`;
 
-export const NavigationBar = () => (
-
-    <Navbar className="color-nav" color="light" expand="lg">
-        <Navbar.Brand href="/Home/Schedule">Schedule</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        {/* <Form className="form-center">
+export function NavigationBar() {
+    return (
+        <div>
+            <Navbar className style={{ backgroundColor: '#3c619a' }}>
+                <Navbar.Brand style={{ color: '#fff' }}  href="/Home/Schedule">Schedule</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                {/* <Form className="form-center">
           <FormControl type="text" placeholder="Search" className="" />
         </Form> */}
-        <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto">
-                <Nav.Item><Nav.Link href="/Home/Homecontent">Home</Nav.Link></Nav.Item>
-                <Nav.Item><Nav.Link href="/Home/Contact">Contact</Nav.Link></Nav.Item>
-            </Nav>
-        </Navbar.Collapse>
-    </Navbar>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto">
+                        <Nav.Item><Nav.Link style={{ color: '#fff' }} href="/Home/Homecontent">Home</Nav.Link></Nav.Item>
+                        <Nav.Item><Nav.Link style={{ color: '#fff' }} href="/Home/Contact">Contact</Nav.Link></Nav.Item>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+    );
+}
 
-)
+export default NavigationBar;
