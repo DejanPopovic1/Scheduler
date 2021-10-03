0069ce676a9ad88ddbe20b170c86bff82e5598cd
DejanPopovic <dejan.dp.popovic@gmail.com>
2021-10-03 21:58:17 +0200

    implemented protected route pt2

--- Scheduler/ClientApp/src/ProtectedRoute.js
+++ Scheduler/ClientApp/src/ProtectedRoute.js
@@ -2,14 +2,12 @@
 import { Redirect, Route } from "react-router-dom";
 
 function ProtectedRoute({ component: Component, ...restOfProps }) {
-    const isAuthenticated = localStorage.getItem("isAuthenticated");
-    console.log("this", isAuthenticated);
-
+    const isAuthenticated = sessionStorage.getItem("token");
     return (
         <Route
             {...restOfProps}
             render={(props) =>
-                isAuthenticated ? <Component {...props} /> : <Redirect to="/signin" />
+                isAuthenticated ? <Component {...props} /> : <Redirect to="/login" />
             }
         />
     );
