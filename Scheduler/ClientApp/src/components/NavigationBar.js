0b5c4a385858b514d26afd25b280fe6b506ef617
DejanPopovic <dejan.dp.popovic@gmail.com>
2021-10-06 23:09:26 +0200

    frontend logout functionality implemented

--- Scheduler/ClientApp/src/components/NavigationBar.js
+++ Scheduler/ClientApp/src/components/NavigationBar.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Nav, Navbar, Form, FormControl } from 'react-bootstrap';
+import { useHistory } from "react-router-dom";
 import './NavBarStyle.css';
 
 export function NavigationBar() {
+    const history = useHistory();
+    function handleLogout() {
+        sessionStorage.clear();
+        history.push('/login');
+    }
     return (
         <div>
             <Navbar className="color-nav">
@@ -12,6 +18,7 @@ export function NavigationBar() {
                     <Nav className="ml-auto">
                         <Nav.Item><Nav.Link style={{ color: '#fff' }} href="/Home">Home</Nav.Link></Nav.Item>
                         <Nav.Item><Nav.Link style={{ color: '#fff' }} href="/Contact">Contact</Nav.Link></Nav.Item>
+                        <Nav.Item><Nav.Link style={{ color: '#fff' }} onClick={handleLogout}>Logout</Nav.Link></Nav.Item>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
