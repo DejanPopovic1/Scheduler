cfae69c9fdd7447587cfa483bfe4bc570f9a3127
DejanPopovic <dejan.dp.popovic@gmail.com>
2021-10-12 18:15:45 +0200

    post in code data to DB

--- Scheduler/Controllers/ScheduleController.cs
+++ Scheduler/Controllers/ScheduleController.cs
@@ -2,6 +2,7 @@
 using Scheduler.Data;
 using Scheduler.Models;
 using Scheduler.Repository;
+using System;
 
 namespace Scheduler.Controllers
 {
@@ -11,22 +12,32 @@ namespace Scheduler.Controllers
     {
         private readonly SchedulerEntities _dbContext;
         private readonly ScheduleRepository _scheduleRepo;
-        public ScheduleController(SchedulerEntities dbContext, ScheduleRepository scheduleRepository)
+        public ScheduleController(SchedulerEntities dbContext/*, ScheduleRepository scheduleRepository*/)
         {
             _dbContext = dbContext;
-            _scheduleRepo = scheduleRepository;
+            //_scheduleRepo = scheduleRepository;
         }
 
         [HttpPost("add")]
         public IActionResult add([FromBody] ScheduleViewModel s)
         {
-            var test1 = 1;
-            _scheduleRepo.Add(_dbContext, s);
+            Schedule ret = new Schedule()
+            {
+                Id = "1",
+                PickupDateTime = new DateTime(2000, 1, 1),
+                ScheduleName = "Hello",
+                Latitude = 2.456F,
+                Longitude = 74.78F
+            };
+            _dbContext.Schedules.Add(ret);
+            _dbContext.SaveChanges();
+            //var test1 = 1;
+            //_scheduleRepo.Add(_dbContext, s);
 
-            s.pickupDate.AddHours(2);
-            //_dbContext.Schedules.Add(s);
-            //var test = s.scheduleName;
-            //var test2 = s.location;
+            //s.pickupDate.AddHours(2);
+            ////_dbContext.Schedules.Add(s);
+            ////var test = s.scheduleName;
+            ////var test2 = s.location;
             return Ok();
         }
     }
