160a4105de421a8ce681edf1a7791afded1b42c8
DejanPopovic <dejan.dp.popovic@gmail.com>
2021-11-09 20:49:11 +0200

    return IActionResult in login controller

--- Scheduler/Controllers/LoginController.cs
+++ Scheduler/Controllers/LoginController.cs
@@ -26,7 +26,7 @@ namespace Scheduler.Controllers
         }
 
         [HttpPost("login")]
-        public ExampleResponse Login([FromBody] Example s)
+        public IActionResult Login([FromBody] Example s)
         {
             ExampleResponse ret = new ExampleResponse(s, "");
             //System.Web.HttpContext.Current.Session["name"] = "Any Name";
@@ -45,9 +45,14 @@ namespace Scheduler.Controllers
                 var user = _dbContext.Users.Where(x => EF.Functions.Collate(x.UserName, "SQL_Latin1_General_CP1_CS_AS") == s.username && x.PasswordHash == hashedPassword).Select(x => new { id = x.Id }).ToList().First();
                 userId = Int32.Parse(user.id);
                 ret = _userService.Authenticate(s, userId);
+                return Ok(ret);
+            }
+            else 
+            {
+                return BadRequest(new { message = "Username or password is incorrect"});
             }
             //return userId;
-            return ret;
+            
         }
 
         [HttpPost("postSchedule")]
