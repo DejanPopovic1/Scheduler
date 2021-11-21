2e5c4fdc5f72fecae5035ccbf017128ae62feaba
DejanPopovic <dejan.dp.popovic@gmail.com>
2021-11-21 11:12:06 +0200

    implemented ScheduleModel

---
+++ Scheduler/Models/ScheduleModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Scheduler.Models
+{
+    public class ScheduleModel
+    {
+        TimeSpan []schedule;
+
+        public ScheduleModel(TimeSpan start, TimeSpan end)
+        {
+            schedule = new TimeSpan[2];
+            schedule[0] = start;
+            schedule[1] = end;
+        }
+
+        public bool isScheduleOverlap(ScheduleModel otherSchedule)
+        {
+            if (isTimeWithinTimePeriod(schedule[0], otherSchedule) || isTimeWithinTimePeriod(schedule[1], otherSchedule))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool isTimeWithinTimePeriod(TimeSpan time, ScheduleModel timePeriod)
+        {
+            if (time > timePeriod.schedule[0] && time < timePeriod.schedule[1]) 
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
