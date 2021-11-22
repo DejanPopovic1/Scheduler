01e20559a1b8ec9d79761257b59e2834e4923255
DejanPopovic <dejan.dp.popovic@gmail.com>
2021-11-23 00:12:24 +0200

    redone unit tests

--- SchedulerTest/UnitTest1.cs
+++ SchedulerTest/UnitTest1.cs
@@ -7,43 +7,34 @@ namespace SchedulerTest
 {
     public class Tests
     {
-        ScheduleModel schedule1;
-        Vertex<ScheduleModel> schedule2;
-        ScheduleModel schedule3;
-        Vertex<ScheduleModel> schedule4;
-        Vertex<ScheduleModel> schedule5;
-        Vertex<ScheduleModel> schedule6;
-        Vertex<ScheduleModel> schedule7;
-        List<Vertex<ScheduleModel>> scheduleModels;
+        List<Vertex> scheduleVertices;
 
         [SetUp]
         public void Setup()
         {
-            schedule1 = new ScheduleModel(new TimeSpan(10, 15, 0), new TimeSpan(11, 0, 0));
-            schedule2 = new ScheduleModel(new TimeSpan(11, 15, 0), new TimeSpan(12, 0, 0));
-            schedule3 = new ScheduleModel(new TimeSpan(11, 10, 0), new TimeSpan(12, 30, 0));
-            schedule4 = new ScheduleModel(new TimeSpan(12, 15, 0), new TimeSpan(12, 45, 0));
-            schedule5 = new ScheduleModel(new TimeSpan(12, 15, 0), new TimeSpan(13, 0, 0));
-            schedule6 = new ScheduleModel(new TimeSpan(12, 30, 0), new TimeSpan(14, 0, 0));
-            schedule7 = new ScheduleModel(new TimeSpan(13, 45, 0), new TimeSpan(14, 30, 0));
+            Vertex scheduleVertex1 = new ScheduleVertex(new ScheduleModel(new TimeSpan(10, 15, 0), new TimeSpan(11, 0, 0)));
+            Vertex scheduleVertex2 = new ScheduleVertex(new ScheduleModel(new TimeSpan(11, 15, 0), new TimeSpan(12, 0, 0)));
+            Vertex scheduleVertex3 = new ScheduleVertex(new ScheduleModel(new TimeSpan(11, 10, 0), new TimeSpan(12, 30, 0)));
+            Vertex scheduleVertex4 = new ScheduleVertex(new ScheduleModel(new TimeSpan(12, 15, 0), new TimeSpan(12, 45, 0)));
+            Vertex scheduleVertex5 = new ScheduleVertex(new ScheduleModel(new TimeSpan(12, 15, 0), new TimeSpan(13, 0, 0)));
+            Vertex scheduleVertex6 = new ScheduleVertex(new ScheduleModel(new TimeSpan(12, 30, 0), new TimeSpan(14, 0, 0)));
+            Vertex scheduleVertex7 = new ScheduleVertex(new ScheduleModel(new TimeSpan(13, 45, 0), new TimeSpan(14, 30, 0)));
 
-            var testList = new List<Vertex<ScheduleModel>>() { schedule3 };
+            scheduleVertex2.AddEdges(new List<Vertex>() { scheduleVertex3 });
+            scheduleVertex3.AddEdges(new List<Vertex>() { scheduleVertex2, scheduleVertex4, scheduleVertex5 });
+            scheduleVertex4.AddEdges(new List<Vertex>() { scheduleVertex3, scheduleVertex5, scheduleVertex6 });
+            scheduleVertex5.AddEdges(new List<Vertex>() { scheduleVertex3, scheduleVertex4, scheduleVertex6 });
+            scheduleVertex6.AddEdges(new List<Vertex>() { scheduleVertex4, scheduleVertex5, scheduleVertex7 });
+            scheduleVertex7.AddEdges(new List<Vertex>() { scheduleVertex6 });
 
-            schedule2.AddEdges(new List<Vertex<ScheduleModel>>() { schedule3 });
-            schedule3.AddEdges(new List<Vertex<ScheduleModel>>() { schedule2, schedule4, schedule5 });
-            schedule4.AddEdges(new List<Vertex<ScheduleModel>>() { schedule3, schedule5, schedule6 });
-            schedule5.AddEdges(new List<Vertex<ScheduleModel>>() { schedule3, schedule4, schedule6 });
-            schedule6.AddEdges(new List<Vertex<ScheduleModel>>() { schedule4, schedule5, schedule7 });
-            schedule7.AddEdges(new List<Vertex<ScheduleModel>>() { schedule6 });
-
-            scheduleModels = new List<Vertex<ScheduleModel>>() {
-                schedule1,
-                schedule2,
-                schedule3,
-                schedule4,
-                schedule5,
-                schedule6,
-                schedule7,
+            scheduleVertices = new List<Vertex>() {
+                scheduleVertex1,
+                scheduleVertex2,
+                scheduleVertex3,
+                scheduleVertex4,
+                scheduleVertex5,
+                scheduleVertex6,
+                scheduleVertex7,
             };
         }
 
