48f00e867af1ba7fbfa509ce489e4ab1ffc34863
DejanPopovic <dejan.dp.popovic@gmail.com>
2021-11-26 15:53:58 +0200

    return dummy getInfo controller data

--- Scheduler/Controllers/SchedulerToolController.cs
+++ Scheduler/Controllers/SchedulerToolController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Scheduler.Data;
 using Scheduler.Models;
+using System;
+using System.Collections.Generic;
 
 namespace Scheduler.Controllers
 {
@@ -18,6 +20,32 @@ namespace Scheduler.Controllers
         [HttpGet("getInfo")]
         public IActionResult GetInfo()
         {
+            SchedulerToolViewModel result = new SchedulerToolViewModel()
+            {
+                NumberOfSchedulingUsers = 4,
+                TotalKmTravelledForTheNextDay = 10000,
+                TotalHoursTravelledForTheNextDay = 2100,
+                GraphDataPoints = new DataPoints() {
+                    DataPointsList = new List<DataPoint>() {
+                        new DataPoint(){
+                            Date = new DateTime(2022, 5, 5),
+                            NumberOfResources = 5,
+                        },
+                        new DataPoint(){
+                            Date = new DateTime(2022, 6, 1),
+                            NumberOfResources = 5,
+                        },
+                        new DataPoint(){
+                            Date = new DateTime(2022, 7, 25),
+                            NumberOfResources = 5,
+                        },
+                    }
+                },
+                HubLocation = new Coordinates() { 
+                    lat = 22.788,
+                    lon = 23.214
+                }
+            };
             return Ok();
         }
 
