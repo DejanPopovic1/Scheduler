c8bde4cdab91062f4bb927c4a3406c05ab9f58e1
DejanPopovic <dejan.dp.popovic@gmail.com>
2021-11-28 17:42:24 +0200

    added backend client to google API call

--- Scheduler/Models/ScheduleCreator.cs
+++ Scheduler/Models/ScheduleCreator.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Cryptography.Xml;
 using System.Threading.Tasks;
 
 namespace Scheduler.Models
 {
+    public class DataObject
+    {
+        public string name { get; set; }
+        public string age { get; set; }
+        public string car { get; set; }
+    }
+
+
     public class ScheduleCreator
     {
+        private const string URL = "https://webhook.site/7dfb72bc-475e-4f19-bb8b-e94bb23eb6d1";
+        private static string urlParameters = "?api_key=123";
         public ScheduleCreator()
         { 
         
@@ -17,6 +30,33 @@ namespace Scheduler.Models
             TimeSpan travelTime = new TimeSpan(0, 1, 0);
             TimeSpan destinationTime = originTime + travelTime;
             ScheduleModel ret = new ScheduleModel(originTime, destinationTime);
+            return ret;
+        }
+
+        public static TimeSpan CalculateTravelTime(float originLat, float originLon, float destinationLat, float destinationLon)
+        {
+            TimeSpan ret = new TimeSpan();
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(URL);
+            // Add an Accept header for JSON format.
+            client.DefaultRequestHeaders.Accept.Add(
+            //new MediaTypeWithQualityHeaderValue("application/json"));
+            new MediaTypeWithQualityHeaderValue("text/json"));
+            HttpResponseMessage response = client.GetAsync(urlParameters).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                // Parse the response body.
+                var dataObjects = response.Content.ReadAsAsync<IEnumerable<DataObject>>().Result;  //Make sure to add a reference to System.Net.Http.Formatting.dll
+                foreach (var d in dataObjects)
+                {
+                    var test = d.age;
+                    Console.WriteLine("{0}", d.age);
+                }
+            }
+            else
+            {
+                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+            }
             return ret;
         }
     }
---
+++ SchedulerTest/ScheduleCreatorTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using Scheduler.Models;
+using System.Collections.Generic;
+using System;
+
+namespace SchedulerTest
+{
+    public class ScheduleCreatorTest
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void should_make_an_api_call()
+        {
+            var test = ScheduleCreator.CalculateTravelTime(1.1f, 2.2f, 3.3f, 4.4f);
+        }
+    }
+}
