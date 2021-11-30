c240810eb70e8ac622e0310789e01a680f0c20f5
DejanPopovic <dejan.dp.popovic@gmail.com>
2021-11-30 17:33:30 +0200

    added implementations to ScheduleCreator

--- Scheduler/Models/ScheduleCreator.cs
+++ Scheduler/Models/ScheduleCreator.cs
@@ -31,10 +31,18 @@ namespace Scheduler.Models
         public string value { get; set; }
     }
 
+    //+-----------------------------------------------------------------------------------------------------------+
+    // Google Distance Matrix API Documentation:                                                                  |
+    // https://developers.google.com/maps/documentation/distance-matrix/overview?hl=en_US                         |
+    //------------------------------------------------------------------------------------------------------------+
+
     public class ScheduleCreator
     {
         private const string URL = "https://maps.googleapis.com/maps/api/distancematrix/json";
         private static string urlParameters = "?destinations=40.598566%2C-73.7527626%7C40.598566%2C-73.7527626&origins=40.6655101%2C-73.89188969999998&key=AIzaSyDc6llaTb4Zxg0whfiuluFdH7RG8z16Gko";
+        private const string commaDelimiter = "%2C";
+        private const string pipeDelimiter = "%7C";
+
 
         public ScheduleCreator()
         { 
@@ -69,6 +77,39 @@ namespace Scheduler.Models
                 Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
             }
             return ret;
+        }
+
+        public static TimeSpan[,] GenerateDistanceMatrix(List<Location> originAddresses, List<Location> destinationAddresses, string APIKey)
+        {
+
+            TimeSpan[,] result = new TimeSpan[originAddresses.Count, destinationAddresses.Count];
+            return result;
+            
+        }
+
+        private static string CreateURLParameterString(List<Location> originAddresses, List<Location> destinationAddresses, string APIKey)
+        {
+            string destinations = "0";
+            string origins = "1";
+            string URLParameters = String.Format("?destinations={0}&origins={1}&key={2}", destinations, origins, APIKey);
+            List<string> originAddressesGoogleAPIFormat = new List<string>();
+            List<string> destinationAddressesGoogleAPIFormat = new List<string>();
+            foreach (Location originAddress in originAddresses)
+            {
+                originAddressesGoogleAPIFormat.Add(ToGoogleAPILocation(originAddress));
+            }
+            foreach (Location destinationAddress in destinationAddresses)
+            {
+                destinationAddressesGoogleAPIFormat.Add(ToGoogleAPILocation(destinationAddress));
+            }
+            string originParameter = String.Join(pipeDelimiter, originAddressesGoogleAPIFormat);
+            string destinationParameter = String.Join(pipeDelimiter, destinationAddressesGoogleAPIFormat);
+            return String.Format("?destinations={0}&origins={1}&key={2}", destinationParameter, originParameter, APIKey);
+        }
+
+        private static string ToGoogleAPILocation(Location location)
+        {
+            return (location.lat).ToString() + commaDelimiter + (location.lon).ToString();
         }
     }
 }
