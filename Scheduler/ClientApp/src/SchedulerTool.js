73e8018257f80902f7330168b001938f68c573f4
DejanPopovic <dejan.dp.popovic@gmail.com>
2021-12-04 00:05:28 +0200

    added table component with dummy data to frontend

--- Scheduler/ClientApp/src/SchedulerTool.js
+++ Scheduler/ClientApp/src/SchedulerTool.js
@@ -12,6 +12,46 @@ import Container from 'react-bootstrap/Container';
 import { PureComponent } from 'react';
 import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, BarChart, Legend, Bar} from 'recharts';
 import CountUp from 'react-countup';
+import MaterialTable from "material-table";
+//import { AddBox, ArrowDownward } from "@material-ui/icons";
+
+import { forwardRef } from 'react';
+
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
+
+const tableIcons = {
+    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+  };
 
 const monthNames = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"
@@ -53,7 +93,8 @@ debugger;
         this.setState({numberOfSchedulingUsers: response.numberOfSchedulingUsers, 
             totalKmTravelledForTheNextDay: response.totalKmTravelledForTheNextDay, 
             totalMinutesTravelledForTheNextDay: response.totalMinutesTravelledForTheNextDay, 
-            requiredResourcesPerDay: initialStateGraph}, //requiredResourcesPerDay
+            requiredResourcesPerDay: initialStateGraph,
+            bookingAssignmentsFirstDay: response.bookingAssignments}, //requiredResourcesPerDay
             this.afterStateUpdate);
     }
 
@@ -255,7 +296,8 @@ debugger;
                                 </AreaChart>
                         </row> */}
 
-                        <row>
+                        <Row>
+                            <Col>
                             Required vehicle resources for the upcoming days:
                             <BarChart width={730} height={250} data={dataSource}>
                             <CartesianGrid strokeDasharray="3 3" />
@@ -265,11 +307,31 @@ debugger;
                             <Legend />
                             <Bar dataKey="value" fill="#8884d8" />
                             </BarChart>
-                        </row>
+                            </Col>
+                        </Row>
 
 
 
-
+                        <Row>
+                            <Col>
+                        <MaterialTable
+                        icons={tableIcons}
+                            columns={[
+                                { title: "Logistic Resource Number", field: "resourceNumber" },
+                                { title: "Start Time", field: "startTime" },
+                                { title: "End Time", field: "endTime"},
+                            ]}
+                            data={[
+                                {
+                                resourceNumber: 1,
+                                startTime: "12:00",
+                                endTime: "15:00",
+                                },
+                            ]}
+                            title="Resource Assignments For The Day"
+                        />
+                        </Col>
+                        </Row>
 
 
                         {/* <XAxis dataKey="date" /> */}
